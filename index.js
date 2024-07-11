ae1f816593fbb4afe5d5ec2cbb1b398ce5aa9676
hani <hani@DESKTOP-5I66P1U.localdomain>
2024-07-11 11:59:34 +0300

    using-array-reuced

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+// index.js
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]; // Provided array of battery amounts
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+console.log(totalBatteries); // To verify the result
